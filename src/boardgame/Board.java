33f6f3efc3b8665148f537f779e4371d88d16270
Matheus Germano <silvaa.matheus112@gmail.com>
2024-07-02 17:41:16 -0300

    Movendo peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -49,6 +49,18 @@ public class Board {
 		
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("A posição não existe na tabela.");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 	private boolean positionExists(int row, int column) {
 		return row >=0 && row<rows && column >=0 && column<columns;
 	}
